d030e121686d512a11c7747b6f02d0495cc33021
prateekpandeycr <prateek@letsmobility.com>
2022-11-02 14:20:27 +0530

    get and key checks for hash table

--- data-structures-in-python-js/hash-table/hash-table.js
+++ data-structures-in-python-js/hash-table/hash-table.js
@@ -18,7 +18,32 @@ class HashTable {
         this.dataMap[index].push([key, value])
         return this
     }
+
+    get(key) {
+        let index = this._hash(key)
+        if(this.dataMap[index]) {
+            for(let i = 0; i < this.dataMap[index].length; i++) {
+                if(this.dataMap[index][i][0] === key) {
+                    return this.dataMap[index][i][1]
+                }
+             }
+         }
+         return undefined
+     }
+
+    keys() {
+        let allKeys = []
+        for(let i = 0; i < this.dataMap.length; i++) {
+            if(this.dataMap[i]) {
+                for(let j = 0; j < this.dataMap[i].length; j++) {
+                    allKeys.push(this.dataMap[i][j][0])
+                }
+            }
+        }
+        return allKeys
+    }
 }
 
 let myHashTable = new HashTable()
-myHashTable
+myHashTable.set('bolts', 1400)
+myHashTable.set('washers', 50)
